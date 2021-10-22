53528481692a0b468e8f0498e8ec13ed3429e906
Rafael Soares de Oliveira <r.soaresiliveira87@gmail.com>
2021-10-21 21:47:35 -0300

    Create Page GoT

--- front-app/src/Pages/GOT.js
+++ front-app/src/Pages/GOT.js
@@ -3,6 +3,26 @@
 import Grid from "../components/Grid/Grid"
 import Colum from "../components/Grid/Colum"
 import ListGroup from "../components/ListGroup"
+import Accordion from "../components/Accordion/Index"
+import BtnAcordion1 from "../components/Accordion/ButtonAcordion/ButtonAcordion1"
+import Collapse1 from "../components/Accordion/AccordionCollapse/Collapse1"
+import BtnAcordion2 from "../components/Accordion/ButtonAcordion/ButtonAcordion2"
+import Collapse2 from "../components/Accordion/AccordionCollapse/Collapse2"
+import BtnAcordion3 from "../components/Accordion/ButtonAcordion/ButtonAcordion3"
+import Collapse3 from "../components/Accordion/AccordionCollapse/Collapse3"
+import BtnAcordion4 from "../components/Accordion/ButtonAcordion/ButtonAcordion4"
+import Collapse4 from "../components/Accordion/AccordionCollapse/Collapse4"
+import BtnAcordion5 from "../components/Accordion/ButtonAcordion/ButtonAcordion5"
+import Collapse5 from "../components/Accordion/AccordionCollapse/Collapse5"
+import BtnAcordion6 from "../components/Accordion/ButtonAcordion/ButtonAcordion6"
+import Collapse6 from "../components/Accordion/AccordionCollapse/Collapse6"
+import BtnAcordion7 from "../components/Accordion/ButtonAcordion/ButtonAcordion7"
+import Collapse7 from "../components/Accordion/AccordionCollapse/Collapse7"
+import BtnAcordion8 from "../components/Accordion/ButtonAcordion/ButtonAcordion8"
+import Collapse8 from "../components/Accordion/AccordionCollapse/Collapse8"
+import HistoryGoT from "../components/GotHistory/History/HistoryGoT"
+
+
 
 
 const GOT = () => {
@@ -11,29 +31,32 @@ const GOT = () => {
 
 
             <br/> <br/> <br/>
-            <h1 className="card-title text-center">Universo Game Oh Thrones (As Crônicas de Gelo e Fogo)</h1>
-            <h5 className="card-text text-center"> A Song of Ice and Fire) é uma série de livros de fantasia épica escrita pelo romancista e roteirista norte-americano <br/> <br/>
-            </h5>
+            <h1 className="card-title text-center">(As Crônicas de Gelo e Fogo)</h1>
+                    <p className="card-text text-center"> 
+                        A Song of Ice and Fire (Brasil: As Crônicas de Gelo e Fogo /Portugal: 
+                        As Crónicas de Gelo e Fogo) é uma série de livros de alta fantasia escrita pelo romancista
+                        e roteirista norte-americano George R. R. Martin.
+                    </p>
 
 
             <Grid>
 
                 <Colum>
                     <ListGroup>
-                        <a href="#HistoriaTerraMedia" class="list-group-item list-group-item-action">História</a>
-                        <a href="#PovosDaTerraMedia" class="list-group-item list-group-item-action">Personagens</a>
-                        <a href="#Linguas" class="list-group-item list-group-item-action">Casas</a>
-                        <a href="#OSilmarillion" class="list-group-item list-group-item-action">Série de TV</a>
+                        <a href='#Historia' class="list-group-item list-group-item-action">História</a>
+                        <a href='#Personagens' class="list-group-item list-group-item-action">Personagens</a>
+                        <a href='#Casas' class="list-group-item list-group-item-action">Casas</a>
+                        <a href="#SerieTv" class="list-group-item list-group-item-action">Série de TV</a>
                     </ListGroup>
                     <br/>
                 </Colum>
                 <br/>
                 <Colum>
                     <ListGroup>
-                        <a href="#MagosAzuis" class="list-group-item list-group-item-action">Livros</a>
-                        <a href="#Elfos" class="list-group-item list-group-item-action">Cultura</a>
-                        <a href="#FilhosDeIluvatar" class="list-group-item list-group-item-action">Temas e inspiração</a>
-                        <a href="#Numenor" class="list-group-item list-group-item-action">Outras obras</a>
+                        <a href='#livros' class="list-group-item list-group-item-action">Livros</a>
+                        <a href='#Cultura' class="list-group-item list-group-item-action">Cultura</a>
+                        <a href='#temas' class="list-group-item list-group-item-action">Temas e inspiração</a>
+                        <a href='#obras' class="list-group-item list-group-item-action">Outras obras</a>
                         
                     </ListGroup>
                     <br/>
@@ -42,6 +65,47 @@ const GOT = () => {
 
                 
             </Grid>
+            <hr/>
+            <Accordion>
+                <div id='Historia' className='TitleAccordion'>
+                    <BtnAcordion1>História</BtnAcordion1>
+                    <Collapse1>
+                            <HistoryGoT></HistoryGoT>
+                    </Collapse1>
+                </div>
+                <div id='Personagens'className='TitleAccordion'>
+                    <BtnAcordion2>Principais Personagens</BtnAcordion2>
+                    <Collapse2></Collapse2>
+                </div>
+                <div id='Casas'className='TitleAccordion'>
+                    <BtnAcordion3>As prinncipais casas</BtnAcordion3>
+                    <Collapse3></Collapse3>
+                </div>
+                <div id='SerieTv'className='TitleAccordion'>
+                    <BtnAcordion4>Série de TV</BtnAcordion4>
+                    <Collapse4></Collapse4>
+                </div>
+                <div id='livros'className='TitleAccordion'>
+                    <BtnAcordion5>Obras literárias</BtnAcordion5>
+                    <Collapse5></Collapse5>
+                </div>
+                <div id='Cultura'className='TitleAccordion'>
+                    <BtnAcordion6>As Cronicas de Gelo e Fog na cultura popular</BtnAcordion6>
+                    <Collapse6></Collapse6>
+                </div>
+                <div id='temas'className='TitleAccordion'>
+                    <BtnAcordion7>Principais Temas e inspiração</BtnAcordion7>
+                    <Collapse7></Collapse7>
+                </div>
+                <div id='obras'className='TitleAccordion'>
+                    <BtnAcordion8>Outras obras derivadas</BtnAcordion8>
+                    <Collapse8></Collapse8>
+                </div>
+                
+                
+                
+            </Accordion>
+            
             
         
 
---
+++ front-app/src/components/GotHistory/History/HistoryGoT.js
@@ -0,0 +1,62 @@
+
+const HistoryGoT = ()=>{
+    return( 
+        <>
+
+            
+
+                
+                
+                
+
+                
+                    <div className="card-group">
+                        <div className="card">
+                            <div className="card-body">
+                                <p className="card-text">
+                                        Os livros narram os acontecimentos dos Sete Reinos, onde uma 
+                                        guerra pela posse do Trono de Ferro é travada. A história se desenvolve
+                                        principalmente no continente de Westeros, cuja extensão é equivalente à
+                                        da América do Sul, mas também em uma extensa terra ao leste chamada Essos. 
+                                        Em Westeros fala-se uma língua comum, que possui vários dialetos regionais.
+                                        Em Essos falam-se diversas línguas, tais como o idioma dothraki, o
+                                        valíriano moderno, dentre outras.
+
+                                    <br/>
+                                    <br/>
+                                        No passado, sete reinos independentes foram tomados, um a um, e
+                                        posteriormente unificados por Aegon e suas irmãs-esposas Rhaenys 
+                                        e Visenya — todos da Casa Targaryen. Embora os Targaryen possuíssem
+                                        um exército pequeno comparado com os dos outros 
+                                        reinos eram a única casa nobre que possuía dragões, o que acabou por lhes dar a maior vantagem.
+                                    <br/>
+                                    
+
+
+
+
+
+
+
+
+                                </p>
+                                <img src="https://cdnb.artstation.com/p/assets/images/images/001/471/233/medium/ertac-altinoz-ooo.jpg?1447016153" class="card-img-top" alt="Povos"></img> <br/> <br/>
+                                <blockquote class="blockquote mb-0">
+                                    <p>Fontes e referecias.</p>
+                                    <footer class="blockquote-footer"><cite title="Source Title">Wikipedia, Wiki Gelo e Fogo</cite></footer>
+                                </blockquote>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                
+            
+
+        </>
+       
+
+    )
+}
+export default HistoryGoT
